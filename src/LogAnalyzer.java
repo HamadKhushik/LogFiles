e9629d5e6ebc1cb9926f336588c186dca04c048c
Hamad <hammadkhan@live.com>
2020-02-19 00:23:45 +0000

    Reading Log Files

---
+++ src/LogAnalyzer.java
@@ -0,0 +1,27 @@
+import edu.duke.FileResource;
+
+import java.util.ArrayList;
+
+public class LogAnalyzer {
+    private ArrayList<LogEntry> records;
+
+    public LogAnalyzer() {
+        records = new ArrayList<LogEntry>();
+    }
+
+    public void readFile(String filename) {
+        FileResource fr = new FileResource(filename);
+        for (String line : fr.lines()) {
+            LogEntry le = WebLogParser.parseEntry(line);
+            records.add(le);
+        }
+    }
+
+    public void printAll() {
+        for (LogEntry le : records) {
+            System.out.println(le);
+        }
+    }
+
+
+}
