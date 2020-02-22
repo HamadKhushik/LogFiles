f8224a9db289760d3d93c8404a7465f323bda30c
Hamad <hammadkhan@live.com>
2020-02-22 17:06:09 +0000

    Methods added : 1. countUniqueIPs 2. printAllHigherThanNum 3. uniqueIPVisitsOnDay 4. countUniqueIPsInRange

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -17,6 +17,61 @@ public class LogAnalyzer {
         }
     }
 
+    public int countUniqueIPs() {
+        //This method should return an integer representing the number of unique IP addresses.
+        // It should also assume that the instance variable records already has its ArrayList
+        // of Strings read in from a file, and should access records in computing this value.
+
+        ArrayList<String> uniqueIPs = new ArrayList<String>();
+        for (LogEntry le : records) {
+            if (!uniqueIPs.contains(le.getIpAddress())) {
+                uniqueIPs.add(le.getIpAddress());
+            }
+        }
+        return uniqueIPs.size();
+    }
+
+    public void printAllHigherThanNum(int num) {
+        //This method should examine all the web log entries in records and print those LogEntrys
+        // that have a status code greater than num.
+
+        for (LogEntry le : records) {
+            if (num < le.getStatusCode()) {
+                System.out.println(le);
+            }
+        }
+    }
+
+    public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
+        //This method accesses the web logs in records and returns an ArrayList of Strings of unique IP
+        // addresses that had access on the given day.
+
+        ArrayList<String> uniqueIPAddrs = new ArrayList<String>();
+        for (LogEntry le : records) {
+            String str = le.getAccessTime().toString();
+            str = str.substring(4, 10);
+            if ((str.equals(someday)) && (!uniqueIPAddrs.contains(le.getIpAddress()))) {
+                uniqueIPAddrs.add(le.getIpAddress());
+            }
+        }
+        return uniqueIPAddrs;
+    }
+
+    public int countUniqueIPsInRange(int low, int high) {
+        // This method returns the number of unique IP addresses in records that have a status code in
+        // the range from low to high, inclusive.
+
+        int inRange = 0;
+        ArrayList<String> uniqueIP = new ArrayList<String>();
+        for (LogEntry le : records) {
+            if (le.getStatusCode() >= low && le.getStatusCode() <= high && !uniqueIP.contains(le.getIpAddress())) {
+                inRange += 1;
+                uniqueIP.add(le.getIpAddress());
+            }
+        }
+        return inRange;
+    }
+
     public void printAll() {
         for (LogEntry le : records) {
             System.out.println(le);
--- src/Tester.java
+++ src/Tester.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Tester {
@@ -14,8 +15,45 @@ public class Tester {
         la.printAll();
     }
 
+    public void testUniqueIPs() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("short-test_log");
+        la.printAll();
+        int IPs = la.countUniqueIPs();
+        System.out.println("The number of unique IP addresses is " + IPs);
+
+    }
+
+    public void testPrintAllHigherThanNum() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("weblog1_log");
+        //la.printAll();
+        la.printAllHigherThanNum(400);
+    }
+
+    public void testUniqueIPVisitsOnDay() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("weblog1_log");
+        ArrayList<String> uniqueIPAddrs = la.uniqueIPVisitsOnDay("Mar 17");
+        for (String IPs : uniqueIPAddrs) {
+            System.out.println(IPs);
+        }
+        System.out.println(uniqueIPAddrs.size());
+    }
+
+    public void testCountUniqueIPsInRange() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("weblog1_log");
+        int inRange = la.countUniqueIPsInRange(200, 299);
+        System.out.println(inRange);
+    }
+
     public static void main(String[] args) {
         Tester t = new Tester();
-        t.testLogAnalyzer();
+        //t.testLogAnalyzer();
+        //t.testUniqueIPs();
+        //t.testPrintAllHigherThanNum();
+        //t.testUniqueIPVisitsOnDay();
+        t.testCountUniqueIPsInRange();
     }
 }
